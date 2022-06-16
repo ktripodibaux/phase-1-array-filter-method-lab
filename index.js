a4018484c917f5224b6fb6d93931c9315882af1e
Kurt Baux <ktripodibaux@gmail.com>
2022-06-16 18:14:12 -0400

    commit tree 92d14feb93f9ae05b4bb2bf4340b0b475ff16512

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+findMatching(drivers, 'Bobby')
+
+function findMatching(array, string) {
+    const matching = array.filter(array => array.toUpperCase() == string.toUpperCase())
+    return matching;
+}
+
+function fuzzyMatch(array, string) {
+    const numLetters = string.length
+    const matching = array.filter(names => names.slice(0,numLetters) == string)
+    return matching
+}
+
+function matchName(array, string) {
+    const matching = array.filter(names => names.name == string)
+    return matching
+}
+
+console.log(fuzzyMatch(drivers, 'Sa'))
